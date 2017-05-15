b7621989703f320d772a768d52cd792815219a29
Paulino Huerta <paulinohuerta@gmail.com>
2017-05-15 22:53:01 +0200

    changing class name

--- MathJunitTestSuite.java
+++ MathJunitTestSuite.java
@@ -2,7 +2,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-   MathTestSolucion.class,
+   MathTest.class,
    TestPerson.class
 })
 public class MathJunitTestSuite {   
